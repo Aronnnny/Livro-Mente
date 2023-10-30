f7ed508c17a0864ec2c58f2618ff32ede1da74af
Aronnnny <aron.cardoso1@gmail.com>
2023-10-30 01:59:24 -0300

    atualization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NextDirective } from './next.directive';
+import { PrevDirective } from './prev.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NextDirective,
+    PrevDirective
   ],
   imports: [
     BrowserModule,
